85773f1f644fb17c291bb417f10704c39f96418b
dor917 <donha917@naver.com>
2023-03-21 20:10:05 +0900

    JPA BASE

--- src/main/java/ex1/jpa/Main.java
+++
@@ -1,5 +0,0 @@
-package ex1.jpa;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,50 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            //insert
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//            em.persist(member);
+//            tx.commit();
+
+            //select
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember = " + findMember.getName());
+//            tx.commit();
+
+            //update
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+//            tx.commit();
+
+            List<Member> result = em.createQuery("SELECT m FROM Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member = " + member.getName());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
